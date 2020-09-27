e067a82f79464b6118ddade9875769f65fb8bc99
neetika-Sri <neetika.srivastava@gmail.com>
2020-09-27 13:30:49 -0700

    Added jar with dependencies and update script to print status

--- bin/run_data_quest.sh
+++ bin/run_data_quest.sh
@@ -3,6 +3,8 @@
 URL="https://download.bls.gov/pub/time.series/pr/"
 BUCKET="rearcquest"
 DEST_FOLDER="./resources" # Must have read write priviledge
-
+ECHO "Fetching Data from $URL"
 clear
 $JAVA_HOME/bin/java -jar data_quest-1.0-SNAPSHOT-jar-with-dependencies.jar $URL ${DEST_FOLDER} $BUCKET
+STATUS=$?
+ECHO "Script Running Status: $STATUS"
